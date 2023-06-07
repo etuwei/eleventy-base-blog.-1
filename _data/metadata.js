0bbd2c1387db1a62d02e918420a76b0bc4351577
etuwei <etuwei@gmail.com>
2023-06-07 13:45:08 +0100

    tasks 1 and 3 from the yellow box on the homepage

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,10 +1,10 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Eleventy Base Blog Portfolio",
+	url: "https://IA-Portfolio.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am posting my work that I have created in the process of learning to write and use HTML, CSS AND JavaScript.",
 	author: {
-		name: "Your Name Here",
+		name: "Ieva Alaunyte",
 		email: "youremailaddress@example.com",
 		url: "https://example.com/about-me/"
 	}
