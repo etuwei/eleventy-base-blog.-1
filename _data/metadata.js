9eaba71f66f52f37698bdffa7a3b3c87f6ead8cf
etuwei <etuwei@gmail.com>
2023-06-08 09:00:26 +0100

    logo

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,5 +1,5 @@
 module.exports = {
-	title: "Eleventy Base Blog Portfolio",
+	title: "Ieva Alaunyte's Portfolio",
 	url: "https://IA-Portfolio.com/",
 	language: "en",
 	description: "I am posting my work that I have created in the process of learning to write and use HTML, CSS AND JavaScript.",
